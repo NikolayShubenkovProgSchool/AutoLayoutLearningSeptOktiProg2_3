b70bb9aac66b453a729c5f116b57ec2e97d17ff5
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2015-10-18 00:35:11 +0300

    initial commit

---
+++ AutoLayoutLearningSeptOktiProg2_3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutLearningSeptOktiProg2_3
+//
+//  Created by Nikolay Shubenkov on 17/10/15.
+//  Copyright © 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
